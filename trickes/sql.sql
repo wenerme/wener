e014040bab7970932a99b9075e18a3abd40ad5cd
wener <wenermail@gmail.com>
2014-12-11 20:05:37 +0800

    add doc

--- trickes/sql.sql
+++ trickes/sql.sql
@@ -56,6 +56,40 @@ select @@datadir;
 -- H2 中这样导出 SQL
 script to 'filenam'
 
+-- ======================================
+-- === 生成测试数据
+-- ======================================
+DROP TABLE IF EXISTS massive;
+/* 不创建索引,否则会产生大量的索引文件 */
+CREATE TABLE massive (id int /*NOT NULL PRIMARY KEY*/ AUTO_INCREMENT, n int, val varchar(40));
+
+-- 使其在一个事务里,增加插入的速度
+DROP PROCEDURE IF EXISTS prepare_data;
+DELIMITER $$
+CREATE PROCEDURE prepare_data (IN n INT)
+BEGIN
+	DECLARE i INT DEFAULT 0;
+	START TRANSACTION;
+	WHILE i < n DO
+		INSERT INTO massive (n,val) VALUES (i, uuid());
+		SET i = i + 1;
+	END WHILE;
+	COMMIT;
+END$$
+DELIMITER ;
+
+CALL prepare_data(100);
+
+SHOW CREATE PROCEDURE prepare_data;
+
+-- 可能需要一些权限
+GRANT EXECUTE ON PROCEDURE test.* TO 'root'@'%';
+flush privileges;
+-- 清除 binlog
+PURGE BINARY LOGS TO 'mysql-bin.010';
+PURGE BINARY LOGS BEFORE '2008-04-02 22:46:26';
+PURGE BINARY LOGS BEFORE now();
+
 -- FAQ
 -- how-to-shrink-purge-ibdata1-file-in-mysql http://stackoverflow.com/questions/3456159/
 -- how-do-i-quickly-rename-a-mysql-database-change-schema-name http://stackoverflow.com/questions/67093/
