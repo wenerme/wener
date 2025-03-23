020ff27d26118b9d91914daa4ab6628bdda9223b
wenerme <wenermail@gmail.com>
2025-03-23 06:14:07 +0000

    Deploying to gh-pages from @ wenerme/wener@11c95a75dbf502f166f16118dfcadcf5b017f1e3 🚀

---
+++ assets/js/a76f40e6.24ca3937.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwener_website=self.webpackChunkwener_website||[]).push([["55142"],{40439:function(e){e.exports=JSON.parse('{"tag":{"label":"ERP","permalink":"/notes/tags/erp","allTagsPath":"/notes/tags","count":1,"items":[{"id":"dev/design/design-erp","title":"Design ERP","description":"- Design API","permalink":"/notes/dev/design/erp"}],"unlisted":false}}')}}]);
