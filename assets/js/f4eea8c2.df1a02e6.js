05eb5c9e58578c3d91d0298bf365ae0b97d68709
wenerme <wenermail@gmail.com>
2025-05-20 07:54:00 +0000

    Deploying to gh-pages from @ wenerme/wener@a5d2b15d77c59379490f1234c70e7c663fc9fef5 🚀

---
+++ assets/js/f4eea8c2.df1a02e6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwener_website=self.webpackChunkwener_website||[]).push([["69674"],{42445:function(e){e.exports=JSON.parse('{"metadata":{"permalink":"/story/page/9","page":9,"postsPerPage":10,"totalPages":10,"totalCount":98,"previousPage":"/story/page/8","nextPage":"/story/page/10","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
